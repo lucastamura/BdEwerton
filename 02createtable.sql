4887b2055faedb4d72c1d28bc5195d3093c1180d
lucastamura <lucas10.tamura@gmail.com>
2023-05-19 15:10:44 -0300

    add: foreing keys

--- 02createtable.sql
+++ 02createtable.sql
@@ -21,12 +21,14 @@ SELECT DATABASE();
 
 -- Tabela pedido_ingrediente
 
-CREATE TABLE pedido_ingrediente(
+CREATE TABLE pedidos_ingredientes(
     codigo_ingrediente INT NOT NULL,  --FK PK
     codigo_fornecedor INT NOT NULL,  --FK PK
     quantidade INT NOT NULL,
     valor DECIMAL(10.2) NOT NULL,
+    PRIMARY KEY(codigo_ingrediente, codigo_fornecedor)
 );
+
 
 
 
@@ -48,7 +50,11 @@ CREATE TABLE ingredientes_receitas (
     codigo_ingrediente INT NOT NULL,   --FK PK
     codigo_receita INT NOT NULL,       --FK PK
     quantidade INT NOT NULL,
+
+    PRIMARY KEY(codigo_ingrediente, codigo_receita)
 );
+
+
 
 
 
@@ -80,10 +86,16 @@ CREATE TABLE produtos(
 
 -- Tabela produtos_pedidos
 CREATE TABLE produtos_pedidos(
-    codigo_Produto INT NOT NULL,   --FK PK
+    codigo_produto INT NOT NULL,   --FK PK
     codigo_pedido INT NOT NULL,   --FK PK
     quantidade INT NOT NULL,
+
+    PRIMARY KEY (codigo_produto, codigo_pedido)
+
 );
+
+
+
 
 
 -- Tabelas pedidos
@@ -98,6 +110,7 @@ CREATE TABLE pedidos(
     codigo_funcionario INT NOT NULL,   --FK 
     PRIMARY KEY(id)
 );
+
 
 
 
@@ -147,12 +160,11 @@ CREATE TABLE pagamento(
 -- Tabela tipo_pagamento
 CREATE TABLE tipo_pagamento(
     id INT NOT NULL AUTO_INCREMENT,
-    tipo_pagamento INT NOT NULL,        --FK
+    tipo INT NOT NULL,
     parcela INT NOT NULL,
 
     PRIMARY KEY(id)
 );
-
 
 
 -- Tabela preco
@@ -229,4 +241,24 @@ CREATE TABLE rua(
     CEP VARCHAR(50) NOT NULL,
 );
 
+ALTER TABLE compra ADD CONSTRAINT fk_rua_bairro FOREIGN KEY (codigo_fornecedor) REFERENCES fornecedorer (id);
+ALTER TABLE fornecedores ADD CONSTRAINT fk_rua_fornecedores FOREIGN KEY (rua) REFERENCES rua (id);
+ALTER TABLE cidade ADD CONSTRAINT fk_cidade_estado FOREIGN KEY (codigo_estado) REFERENCES estado (id);
+ALTER TABLE bairro ADD CONSTRAINT fk_bairro_cidade FOREIGN KEY (codigo_cidade) REFERENCES cidade (id);
 ALTER TABLE rua ADD CONSTRAINT fk_rua_bairro FOREIGN KEY (codigo_bairro) REFERENCES bairro (id);
+ALTER TABLE pedidos ADD CONSTRAINT fk_pedido_cliente FOREIGN KEY (codigo_cliente) REFERENCES clientes (id);
+ALTER TABLE pedidos ADD CONSTRAINT fk_pedido_funcionario FOREIGN KEY (codigo_funcionario) REFERENCES funcionarios (id);
+ALTER TABLE clientes ADD CONSTRAINT fk_cliente_rua FOREIGN KEY (rua) REFERENCES rua (id);
+ALTER TABLE pagamento ADD CONSTRAINT fk_codigopagamento_pedidos FOREIGN KEY (codigo_pagamento) REFERENCES pedidos (id);
+ALTER TABLE pagamento ADD CONSTRAINT fk_tipopagamento_pedidos FOREIGN KEY (tipo_pagamento) REFERENCES tipo_pagamento (id);
+ALTER TABLE funcionario ADD CONSTRAINT fk_funcionario_rua FOREIGN KEY (rua) REFERENCES rua (id);
+ALTER TABLE preco ADD CONSTRAINT fk_preco_produto FOREIGN KEY (codigo_produto) REFERENCES produtos (id);
+ALTER TABLE produtos ADD CONSTRAINT fk_pedido_receita FOREIGN KEY (codigo_receita) REFERENCES receitas (id);
+
+
+ALTER TABLE produtos_pedidos ADD CONSTRAINT fk_produto_produtos FOREIGN KEY (codigo_produto) REFERENCES produtos (id);
+ALTER TABLE ingredientes_receitas ADD CONSTRAINT fk_ingrediente_ingredientes FOREIGN KEY (codigo_ingrediente) REFERENCES ingredientes (id);
+ALTER TABLE pedido_ingrediente ADD CONSTRAINT fk_pedido_ingrediente FOREIGN KEY (codigo_ingrediente) REFERENCES ingredientes (id);
+ALTER TABLE produtos_pedidos ADD CONSTRAINT fk_pedido_pedidos FOREIGN KEY (codigo_pedido) REFERENCES pedidos (id);
+ALTER TABLE ingredientes_receitas ADD CONSTRAINT fk_receita_receitas FOREIGN KEY (codigo_receita) REFERENCES receitas (id);
+ALTER TABLE pedido_ingrediente ADD CONSTRAINT fk_pedido_forncedor FOREIGN KEY (codigo_fornecedor) REFERENCES fornecedores (id);
